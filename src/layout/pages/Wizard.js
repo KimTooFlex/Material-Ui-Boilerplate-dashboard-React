bb564c34d090160f7159a845d355da439cc9e85e
unknown <KIM TOO FLEX@KIMTOOFLEX-PC.KTF>
2018-01-17 11:06:15 +0300

    Signed-off-by: unknown <KIM TOO FLEX@KIMTOOFLEX-PC.KTF>

--- src/layout/pages/Wizard.js
+++ src/layout/pages/Wizard.js
@@ -1,16 +1,49 @@
 import React, {Component} from 'react';
 import {subscribe, setState, getState} from "../../lib/Redux";
 import Button from 'material-ui/Button';
+import Stepper, { Step, StepLabel } from 'material-ui/Stepper';
+import Typography from 'material-ui/Typography';
 
 class Wizard extends Component {
     state ={
-       step: "TYPE"
+       step: "TYPE",
+        activeSteps:  {
+            activeStep: 1,
+            skipped: new Set(),
+        }
     }
 
 componentWillMount()
 {
     subscribe("Wizard",this);
 }
+
+
+    getSteps() {
+        return ['Alert Type', 'Compose Message', 'Choose Tags & User Groups'];
+    }
+
+    getStepContent(step) {
+        switch (step) {
+            case 0:
+                return 'Choose Alert Type...';
+            case 1:
+                return 'The SMS you want to broadcast...';
+            case 2:
+                return 'Choose user groups/Tags';
+            default:
+                return 'Unknown step';
+        }
+    }
+
+    isStepOptional = step => {
+        return step === 1;
+    };
+
+    isStepSkipped(step) {
+        return this.state.activeSteps.skipped.has(step);
+    }
+
 
 render() {
 
@@ -48,6 +81,26 @@ render() {
                       <hr/>
                     <div className={"row"}>
                         <div className={"col"}>
+                            <Stepper activeStep={this.state.activeSteps}>
+                                {this.getSteps().map((label, index) => {
+                                    const props = {};
+                                    const labelProps = {};
+
+                                    if (this.isStepSkipped(index)) {
+                                        props.completed = false;
+                                    }
+
+                                    return (
+                                        <Step key={label} {...props}>
+                                            <StepLabel {...labelProps}>{label}</StepLabel>
+                                        </Step>
+                                    );
+                                })}
+                            </Stepper>
+                        </div>
+                    </div>
+                    <div className={"row"}>
+                        <div className={"col"}>
                             {page}
                         </div>
                     </div>
@@ -59,11 +112,12 @@ render() {
 
 
 class Step1 extends React.Component {
-    state = {test: "Hellow"};
+    state = {
+
+    };
 
     componentWillMount() {
         subscribe("Step1", this);
-     
 
     }
 
@@ -75,7 +129,7 @@ class Step1 extends React.Component {
     render() {
         return (
             <div>
-                Hello world
+
             </div>)
     }
 
